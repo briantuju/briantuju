7c7a39a19e950e8eb7c812eb31932428f6f0e1f5
Brian Tuju <briantuju@gmail.com>
2023-04-14 15:13:16 +0300

    Add site colors to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
+      colors: {
+        text: { DEFAULT: '#111827' },
+
+        light: { DEFAULT: '#f3f4f6' },
+      },
     },
   },
   plugins: [],
