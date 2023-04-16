94273542d84bbf600adb5ba8eebea3e2444f1907
Brian Tuju <briantuju@gmail.com>
2023-04-16 12:44:42 +0300

    Implement useWindowSize hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import { NextPage } from 'next'
+import { Size, useWindowSize } from '@/hooks/useWindowSize'
 
 const Home: NextPage = () => {
+  const size: Size = useWindowSize()
+
   return (
     <>
       <Head>
@@ -10,6 +13,10 @@ const Home: NextPage = () => {
 
       <main>
         <h1>Brian Tuju</h1>
+        <br />
+        <p>
+          {size.width}px / {size.height}px
+        </p>
       </main>
     </>
   )
